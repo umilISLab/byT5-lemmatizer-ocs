ff243dd8af82bc96b83cc1ae9247523e626fd845
Martin <martin.ruskov@unimi.it>
2024-07-15 17:19:06 +0200

    Introduced Trainer

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # data_root = "../stanza_pos"
-data_root = "./stanza_pos"
+data_root = "./data"
 
 model_root = "./model"
 
 token_len = 50
 annot_len = 10
 
-checkpoint_name = "byT5-ocs-stanzapos-06"
+checkpoint_name = "byT5-ocs-38"
